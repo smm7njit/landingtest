d393a0255c7088a1b3ce03c920af88130c571cad
Mourtada <smm7@njit.edu>
2023-12-09 20:59:15 -0500

    Add analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { NextUIProvider, createTheme} from '@nextui-org/react';
+import Script from 'next/script';
 
 const darkTheme = createTheme({type: "dark"});
 
@@ -9,6 +10,7 @@ function MyApp({ Component, pageProps }: AppProps) {
     <NextUIProvider theme={darkTheme}>
       <Component {...pageProps} />
     </NextUIProvider>
+    
   )
 }
 
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Document, { Html, Head, Main, NextScript, DocumentContext } from "next/document";
 import { CssBaseline } from "@nextui-org/react";
+import Script from "next/script";
  
 class MyDocument extends Document {
   static async getInitialProps(ctx : DocumentContext) {
@@ -14,7 +15,23 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head>{CssBaseline.flush()}</Head>
+        <Head>{CssBaseline.flush()}
+        <Script
+  strategy="lazyOnload"
+  src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS}`}
+/>
+
+<Script id="ga-script" strategy="lazyOnload">
+  {`
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', '${process.env.GOOGLE_ANALYTICS}', {
+      page_path: window.location.pathname,
+    });
+        `}
+</Script>
+        </Head>
         <body>
           <Main />
           <NextScript />
