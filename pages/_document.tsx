0e176e10d4efa98004bf637356738a773b85208c
Mourtada <smm7@njit.edu>
2023-12-10 23:48:33 -0500

    Edit document.tsx and app.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Document, { Html, Head, Main, NextScript, DocumentContext } from "next/document";
 import { CssBaseline } from "@nextui-org/react";
-import Script from "next/script";
+import Script from "next/script"
 
 declare global {
   interface Window {
@@ -10,30 +10,32 @@ declare global {
 }
 
 
- 
+
 const GA_TRACKING_ID = process.env.GTAG;
 class MyDocument extends Document {
-  static async getInitialProps(ctx : DocumentContext) {
+  static async getInitialProps(ctx: DocumentContext) {
     const initialProps = await Document.getInitialProps(ctx);
     return {
       ...initialProps,
       styles: React.Children.toArray([initialProps.styles]),
     };
   }
- 
+
   render() {
     return (
       <Html lang="en">
         <Head>{CssBaseline.flush()}
 
-        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-DD3RM7V9YG"></Script>
-          <Script>
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){window['dataLayer'].push(arguments)}
-            gtag('js', new Date());
-
-            gtag('config', 'G-DD3RM7V9YG');
+          <Script id="gtag-script" async src="https://www.googletagmanager.com/gtag/js?id=G-DD3RM7V9YG"></Script>
+          <Script id="gtag-inline-script">
+            {`
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){window['dataLayer'].push(arguments)}
+    gtag('js', new Date());
+    gtag('config', 'G-DD3RM7V9YG');
+  `}
           </Script>
+
 
         </Head>
         <body>
@@ -44,5 +46,5 @@ class MyDocument extends Document {
     );
   }
 }
- 
+
 export default MyDocument;
