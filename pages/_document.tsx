9841d56e3e8e507156219fc00b882ddb3b93269b
Mourtada <smm7@njit.edu>
2023-12-11 01:48:25 -0500

    Add gtag.js

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Document, { Html, Head, Main, NextScript, DocumentContext } from "next/document";
 import { CssBaseline } from "@nextui-org/react";
-import Script from "next/script"
+
 
 declare global {
   interface Window {
@@ -11,7 +11,6 @@ declare global {
 
 
 
-const GA_TRACKING_ID = process.env.GTAG;
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
     const initialProps = await Document.getInitialProps(ctx);
@@ -25,19 +24,6 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>{CssBaseline.flush()}
-
-
-          <Script id="gtag-script" async src="https://www.googletagmanager.com/gtag/js?id=G-DD3RM7V9YG"></Script>
-          <Script id="gtag-inline-script" strategy="lazy-onload">
-            {`
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){window.dataLayer.push(arguments);}
-    gtag('js', new Date());
-    gtag('config', 'G-DD3RM7V9YG');
-  `}
-          </Script>
-
-
         </Head>
         <body>
           <Main />
