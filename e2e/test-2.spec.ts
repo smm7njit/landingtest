7084c64e179402f2b7cd01eb1421714dd94db616
Mourtada <smm7@njit.edu>
2023-12-15 03:42:50 -0500

    Add jumbotron playwright test
    Added a playright test whose purpose is to verify the visibility of
    the first jumbotron component.

--- e2e/test-2.spec.ts
+++ e2e/test-2.spec.ts
@@ -34,3 +34,8 @@ test('Check if subtext of hero section is present', async ({ page }) => {
   await page.goto('http://localhost:3000/');
   await page.getByText('We always guarantee that our').click();
 });
+
+test('Check if first jumbotron component is visible', async ({ page }) => {
+  await page.goto('http://localhost:3000/');
+  await page.locator('[id="__next"] div').filter({ hasText: 'When it comes to drinks,we\'re' }).nth(2).click();
+});
