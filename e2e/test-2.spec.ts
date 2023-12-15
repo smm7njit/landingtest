a69d36de450887182b673b969928587a268b51ac
Mourtada <smm7@njit.edu>
2023-12-15 01:21:19 -0500

    Add playwright tests
    Added playwright tests for hero section text, newsletter link and
    newsletter text

---
+++ e2e/test-2.spec.ts
@@ -0,0 +1,16 @@
+import { test, expect } from '@playwright/test';
+
+test('Test hero section text is present', async ({ page }) => {
+  await page.goto('http://localhost:3000/');
+  await page.getByText('When it comes to drinks,').click();
+});
+
+test('Check link to Newsletter works', async ({ page }) => {
+  await page.goto('http://localhost:3000/');
+  await page.getByRole('link', { name: 'More' }).click();
+});
+
+test('Check if newsletter page text is present', async ({ page }) => {
+  await page.goto('http://localhost:3000/');
+  await page.getByRole('heading', { name: 'Sign up for newsletters and' }).click();
+});
