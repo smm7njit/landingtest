15072fc5d7f26ce68d1285c0ffa377256e679631
Mourtada <smm7@njit.edu>
2023-12-18 19:36:35 -0500

    Edit mailchimp

--- components/NewsletterForm.js
+++ components/NewsletterForm.js
@@ -3,7 +3,7 @@ import MailchimpSubscribe from 'react-mailchimp-subscribe';
 
 const NewsletterForm = () => {
   return (
-    <MailchimpSubscribe url={process.env.NEXT_PUBLIC_MAILCHIMP_URL} />
+    <MailchimpSubscribe url='https://njit.us17.list-manage.com/subscribe/post?u=48178868d712618e32c4231b0&amp;id=fdf16ce7d8&amp;f_id=006e84e2f0' />
   );
 };
 
